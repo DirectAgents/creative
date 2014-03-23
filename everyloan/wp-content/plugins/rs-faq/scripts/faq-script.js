43afe1ceb23c0c7c3ceb51cad0b9d36772d9c7c5
Roman Sharf <pencilking2002@gmail.com>
2014-03-23 01:41:40 -0400

    Fixed FAQ fnctionality

--- everyloan/wp-content/plugins/rs-faq/scripts/faq-script.js
+++ everyloan/wp-content/plugins/rs-faq/scripts/faq-script.js
@@ -1,10 +1,22 @@
 $(function() {
 	
 	// Toggle the FAQ
+	var $last_open = false;
+
 	$('.faq-title a').click(function() {
 		
-		$(this).find('span').toggle();
-		$(this).parent().next().slideToggle();
+		var $this = $(this),
+			$parent = $this.parent();
+		
+		// Hide any other questions that are open, so only one question is visible at a time
+		if ($last_open && $last_open.parent().parent().attr('id') != $parent.parent().attr('id'))
+			$last_open.parent().next().slideToggle();
+		
+
+		$this.find('span').toggle();
+		$parent.next().slideToggle();
+		$last_open = $this;
+
 		return false;
 	
 	});
