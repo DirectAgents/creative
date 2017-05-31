727a09cbd998270d2a8f1e5550edd01ec21eeede
alper@directagents.com <alperdilmen@Alpers-iMac.local>
2017-05-31 11:33:25 -0400

    asdfasdf

--- pos/survey/cronjobs/participant/emailnotifications/send_when_you_qualify_for_new_project/index.php
+++ pos/survey/cronjobs/participant/emailnotifications/send_when_you_qualify_for_new_project/index.php
@@ -146,14 +146,6 @@ while($row2 = mysqli_fetch_array($sql2))
 
 
 
-//echo $row['Participant_EmailNotifications'];
-
-$emailnotifications=explode(',',$row['Participant_EmailNotifications']);
-
-if(!in_array($row2['userID'],$emailnotifications)){
-
-
-
 
 
 
@@ -302,7 +294,13 @@ if($Languages != 'NULL' && $Languages != ''){$languages = "AND Languages RLIKE '
 
 
 
-$sql3 = mysqli_query($connecDB,"SELECT * FROM tbl_participant WHERE userID NOT IN (SELECT userID FROM tbl_participant_meeting_participated WHERE ProjectID = '".$row['id']."') AND userID NOT IN (SELECT userID FROM tbl_meeting_request WHERE ProjectID = '".$row['id']."') AND userID NOT IN (SELECT userID FROM tbl_meeting_upcoming WHERE ProjectID = '".$row['id']."') AND userID NOT IN (SELECT userID FROM tbl_meeting_recent WHERE ProjectID = '".$row['id']."') AND userID NOT IN (SELECT userID FROM tbl_meeting_archived_participant WHERE ProjectID = '".$row['id']."' AND Met = 'yes') AND userID NOT IN (SELECT userID FROM tbl_meeting_archived_startup WHERE ProjectID = '".$row['id']."' AND Met = 'yes') $theage $thegender $theheight $thecity $thestatus $theethnicity $thesmoke $thedrink $thediet $thereligion $theeducation $thejob $interests $languages ORDER BY userID DESC");
+$sql3 = mysqli_query($connecDB,"SELECT * FROM tbl_participant WHERE userID NOT IN (SELECT userID FROM tbl_participant_meeting_participated WHERE ProjectID = '".$row['id']."') 
+    AND userID NOT IN (SELECT userID FROM tbl_meeting_request WHERE ProjectID = '".$row['id']."') 
+    AND userID NOT IN (SELECT userID FROM tbl_meeting_upcoming WHERE ProjectID = '".$row['id']."') 
+    AND userID NOT IN (SELECT userID FROM tbl_meeting_recent WHERE ProjectID = '".$row['id']."') 
+    AND userID NOT IN (SELECT userID FROM tbl_meeting_archived_participant WHERE ProjectID = '".$row['id']."' 
+    AND Met = 'yes') AND userID NOT IN (SELECT userID FROM tbl_meeting_archived_startup WHERE ProjectID = '".$row['id']."' 
+    AND Met = 'yes') $theage $thegender $theheight $thecity $thestatus $theethnicity $thesmoke $thedrink $thediet $thereligion $theeducation $thejob $interests $languages ORDER BY userID DESC");
 
 
 
@@ -319,321 +317,22 @@ while($rowparticipant = mysqli_fetch_array($sql3))
     $results[] = $rowparticipant['userID'];
 
 
-echo $rowparticipant['userID'];
-echo "<br>";
+//echo $rowparticipant['userID'];
+//echo "<br>";
 
 
 //echo $row2['userID'];
 
 
+$emailnotifications=explode(',',$row['Participant_EmailNotifications']);
+
+if(!in_array($rowparticipant['userID'],$emailnotifications)){
+
+echo $rowparticipant['userID'];
+
+}
 
 
-
-
-// using SendGrid's PHP Library
-// https://github.com/sendgrid/sendgrid-php
-// If you are using Composer (recommended)
-require '../../../sendgrid-php/vendor/autoload.php';
-// If you are not using Composer
-// require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Meeting you qualify", "support@valifyit.com");
-$subject = "Meeting you qualify";
-$to = new SendGrid\Email($rowparticipant['FirstName'], $rowparticipant['userEmail']);
-$content = new SendGrid\Content("text/html", '
-
-
-<body style="margin: 0 !important; padding: 0 !important;">
-
-
-
-<!-- HEADER -->
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
-    <tr>
-        <td bgcolor="#fdfdfd" align="center">
-            <!--[if (gte mso 9)|(IE)]>
-            <table align="left" border="0" cellspacing="0" cellpadding="0" width="600">
-            <tr>
-            <td align="left" valign="top" width="600">
-            <![endif]-->
-            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-top:50px; max-width: 600px;" class="wrapper">
-                <tr>
-                    <td align="left" valign="top" style="padding:20px;" class="logo">
-                        <a href="http://valifyit.com/" target="_blank">
-                            <img alt="Logo" src="http://valifyit.com/images/email/email-logo-large.jpg" width="132" height="48" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
-                        </a>
-                    </td>
-                </tr>
-            </table>
-            <!--[if (gte mso 9)|(IE)]>
-            </td>
-            </tr>
-            </table>
-            <![endif]-->
-        </td>
-    </tr>
-    
-   
-    <tr>
-        <td bgcolor="#fdfdfd" align="center" style="padding: 10px 15px 30px 15px;" class="section-padding">
-            <!--[if (gte mso 9)|(IE)]>
-            <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-            <tr>
-            <td align="center" valign="top" width="600">
-            <![endif]-->
-            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="background:#fff; padding:20px; border:1px solid #f0f0f0; max-width: 600px;" class="responsive-table">
-                <!-- TITLE -->
-               
-                <tr>
-                  <td align="center" height="100%" valign="top" width="100%" colspan="2">
-                        <!--[if (gte mso 9)|(IE)]>
-                        <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-                        <tr>
-                        <td align="center" valign="top" width="600">
-                        <![endif]-->
-                        
-                     
-
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600;">
-                            <tr>
-                                <td align="center" valign="top" style="font-size:0;">
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-                                    <tr>
-                                    <td align="left" valign="top" width="115">
-                                    <![endif]-->
-                                    <div style="display:inline-block; margin: 0 -2px; max-width:115px; vertical-align:top; width:100%;">
-
-                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="115">
-                                            <tr>
-                                                <td valign="top" style="padding: 40px 0 0 0;" class="mobile-hide"><a href="http://valifyit.com/" target="_blank"><img src="http://valifyit.com/images/email/lightbulb.jpg" alt="idea" width="60" height="55" border="0" style="display: block; font-family: Arial; color: #666666; font-size: 14px; width: 60px; height: 55px;"></a></td>
-                                            </tr>
-                                        </table>
-                                    </div>
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    </td>
-                                    <td align="left" valign="top" width="385">
-                                    <![endif]-->
-                                    <div style="display:inline-block; margin: 0 -2px; max-width:385px; vertical-align:top; width:100%;">
-
-                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
-                                            <tr>
-
-                                                <td style="padding: 40px 0 0 0;" class="no-padding">
-                                                    <!-- ARTICLE -->
-                                                    <table border="0" cellspacing="0" cellpadding="0" width="100%">
-                                                       
-                                                        <tr>
-                                                            <td align="left" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">'.$row['Name'].'</td>
-                                                        </tr>
-                                                        <tr>
-                                                             <td align="left" style="padding: 10px 0 15px 25px; font-size: 16px; line-height: 24px; font-family: Helvetica, Arial, sans-serif; color: #666666;" class="padding">'.$row['Details'].'</td>
-                                                        </tr>
-
-                                                        
-
-                                                    </table>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </div>
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    </td>
-                                    </tr>
-                                    </table>
-                                    <![endif]-->
-                                </td>
-                            </tr>
-                        </table>
-
-
-
-
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600;">
-                            <tbody><tr>
-                                <td align="center" valign="top" style="font-size:0;">
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-                                    <tr>
-                                    <td align="left" valign="top" width="115">
-                                    <![endif]-->
-                                    <div style="display:inline-block; margin: 0 -2px; max-width:115px; vertical-align:top; width:100%;">
-
-                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="115">
-                                            <tbody><tr>
-                                                <td valign="top" style="padding: 40px 0 0 0;" class="mobile-hide"><a href="http://valifyit.com/" target="_blank"><img src="http://valifyit.com/images/email/money.jpg" alt="money" width="60" height="55" border="0" style="display: block; font-family: Arial; color: #666666; font-size: 14px; width: 60px; height:55px;"></a></td>
-                                            </tr>
-                                        </tbody></table>
-                                    </div>
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    </td>
-                                    <td align="left" valign="top" width="385">
-                                    <![endif]-->
-                                    <div style="display:inline-block; margin: 0 -2px; max-width:385px; vertical-align:top; width:100%;">
-
-                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
-                                            <tbody><tr>
-
-                                                <td style="padding: 40px 0 0 0;" class="no-padding">
-                                                    <!-- ARTICLE -->
-                                                    <table border="0" cellspacing="0" cellpadding="0" width="100%">
-                                                        <tbody>
-                                                        <tr>
-                                                            <td align="left" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
-                                                                $'.$row['Pay'].' for '.$row['Minutes'].' minutes of meeting
-                                                                </td>
-                                                        </tr>
-                                                      
-                                                      
-
-                                                    </tbody></table>
-                                                </td>
-                                            </tr>
-                                        </tbody></table>
-                                    </div>
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    </td>
-                                    </tr>
-                                    </table>
-                                    <![endif]-->
-                                </td>
-                            </tr>
-                        </tbody></table>
-                        
-                        
-                        
-                        
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600;">
-                            <tbody><tr>
-                                <td align="center" valign="top" style="font-size:0;">
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-                                    <tr>
-                                    <td align="left" valign="top" width="115">
-                                    <![endif]-->
-                                  
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    </td>
-                                    <td align="left" valign="top" width="385">
-                                    <![endif]-->
-                                   
-                                    <!--[if (gte mso 9)|(IE)]>
-                                    </td>
-                                    </tr>
-                                    </table>
-                                    <![endif]-->
-                                </td>
-                            </tr>
-
-                             <tr>
-                               
-                    <td align="center" style="padding: 20px; font-size: 25px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #ffffff;" class="padding" colspan="2"></td>
-                </tr>
-
-
-                              <tr>
-                               
-                    <td align="center" style="padding: 20px; background:#4c71dc; font-size: 25px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #ffffff;" class="padding" colspan="2"><a href="'.BASE_PATH.'/ideas/p/'.$row['Category'].'/?id='.$row['ProjectID'].'&p='.$rowparticipant['userID'].'" target="_blank" style="font-weight: normal; color: #ffffff;">View Details</a></td>
-                </tr>
-
-
-
-                        </tbody></table>
-
-
-                        <!--[if (gte mso 9)|(IE)]>
-                        </td>
-                        </tr>
-                        </table>
-                        <![endif]-->
-                    </td>
-                </tr>
-              
-               
-            </table>
-
-
-
-
-
-
-
-
-
-
-
-            <!--[if (gte mso 9)|(IE)]>
-            </td>
-            </tr>
-            </table>
-            <![endif]-->
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff" align="center" style="padding: 20px 0px;">
-            <!--[if (gte mso 9)|(IE)]>
-            <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-            <tr>
-            <td align="center" valign="top" width="600">
-            <![endif]-->
-            <!-- UNSUBSCRIBE COPY -->
-
-          
-
-
-               <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="max-width: 600px;" class="responsive-table">
-                <tr>
-                    <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
-                        <img alt="Logo" src="http://valifyit.com/images/email/email-logo-small.jpg" width="110" height="34" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
-                           </td>
-                     </tr>
-
-                   
-            </table>
-
-
-
-            <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="max-width: 600px;" class="responsive-table">
-                <tr>
-                    <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
-                        245 5th Ave Suite 201, New York, NY 10001
-                           </td>
-                     </tr>
-
-                      <tr>
-                      <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">   
-                        <a href="http://valifyit.com/terms/" target="_blank" style="color: #666666; text-decoration: none;">Terms of Service</a> | <a href="http://valifyit.com/privacy/" target="_blank" style="color: #666666; text-decoration: none;">Privacy</a>  | <a href="http://valifyit.com/faq/" target="_blank" style="color: #666666; text-decoration: none;">FAQ</a></td>
-                       
-                        
- 
-                   
-                </tr>
-            </table>
-
-
-
-            <!--[if (gte mso 9)|(IE)]>
-            </td>
-            </tr>
-            </table>
-            <![endif]-->
-        </td>
-    </tr>
-</table>
-
-
-</body>
-</html>
-
-
-
-    ');
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = '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';
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
-//echo $response->statusCode();
-//echo $response->headers();
-//echo $response->body();
 
 
 
@@ -649,6 +348,10 @@ $response = $sg->client->mail()->send()->post($mail);
 
 
 $userID = implode(",", $results);
+
+//echo $row['Participant_EmailNotifications'];
+
+
 
 //echo $userID;
 
@@ -670,7 +373,6 @@ exit();
 
 
 
-}
 
 
 
