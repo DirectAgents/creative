0d3328854adc0f6f2e1ac1fd16fd7f42bff603db
Alper Dilmen <ald183s@gmail.com>
2017-09-16 10:13:37 -0400

    dfasdfa

--- pos/bottle/signup/test/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Login With Facebook</title>
-</head>
-<body>
-  <a href="facebook_login.php"><img src="login-button.png" alt="Facebook Login Button"></a>
-</body>
-</html>
