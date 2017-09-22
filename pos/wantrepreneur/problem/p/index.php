375404c76662266e69e27f22a9c59d1987bff649
Alper Dilmen <ald183s@gmail.com>
2017-09-22 17:25:31 -0400

    dadfasdfa

--- pos/wantrepreneur/problem/p/index.php
+++ pos/wantrepreneur/problem/p/index.php
@@ -475,14 +475,18 @@ if($rowrequest['userID'] == $_SESSION['participantSession'] && $rowrequest['Proj
 <input type="hidden" name="problemid" id="problemid" value="<?php echo $_GET['id']; ?>"/>  
 
 
+<!--Max recording is set to 5 minutes -->
+
 <!-- begin video recorder code -->
 <script type="text/javascript">
 var size = {width:640,height:510};
-var flashvars = {qualityurl: "avq/480p.xml",accountHash:"806aaf1fee6d34f6268b141febc7cba3", eid:1, showMenu:"true", mrt:120,sis:0,asv:1,mv:0};
+var flashvars = {qualityurl: "avq/480p.xml",accountHash:"806aaf1fee6d34f6268b141febc7cba3", eid:1, showMenu:"true", mrt:300,sis:0,asv:1,mv:0};
 (function() {var pipe = document.createElement('script'); pipe.type = 'text/javascript'; pipe.async = true;pipe.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 's1.addpipe.com/1.3/pipe.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pipe, s);})();
 </script>
 <div id="hdfvr-content" ></div>
 <!-- end video recorder code -->
+
+
 
 
 <?php
