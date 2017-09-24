b349d607bce6fd471b363b2c33892667d062f99a
Alper Dilmen <ald183s@gmail.com>
2017-09-24 12:38:12 -0400

    asdasdf

--- pos/wantrepreneur/problem/p/index.php
+++ pos/wantrepreneur/problem/p/index.php
@@ -496,66 +496,38 @@ if($rowrequest['userID'] == $_SESSION['participantSession'] && $rowrequest['Proj
 
 
 
-<!-- Delete a Project -->
+<!-- Possible Answer Question 1 -->
 
-<div id="slide" class="well slide">
+<div id="slide-video-feedback" class="well slide-video-feedback">
  
 
-<div id="PossibleAnswer1_Question1">
-1
+
 <p>
 
-<!--Max recording is set to 5 minutes -->
-
-<!-- begin video recorder code -->
-<script type="text/javascript">
-var size = {width:400,height:330};
-var flashvars = {qualityurl: "avq/300p.xml",accountHash:"806aaf1fee6d34f6268b141febc7cba3", eid:1, showMenu:"true", mrt:300,sis:0,asv:1,mv:0, payload:"{\"userID\":\"<?php echo $_SESSION['participantSession']; ?>\",\"Answer\":\"PossibleAnswer1_Question1\"}"};
-(function() {var pipe = document.createElement('script'); pipe.type = 'text/javascript'; pipe.async = true;pipe.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 's1.addpipe.com/1.3/pipe.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pipe, s);})();
-</script>
-<div id="hdfvr-content" ></div>
-<!-- end video recorder code -->
+ <iframe id="frame-video-feedback" src="" scrolling="no"></iframe>
 
 </p>
-</div>
-
-
-<div id="PossibleAnswer1_Question2">
-<br><br><br>
-2
-<p>
-
-<!--Max recording is set to 5 minutes -->
-
-<!-- begin video recorder code -->
-<script type="text/javascript">
-var size = {width:400,height:330};
-var flashvars = {qualityurl: "avq/300p.xml",accountHash:"806aaf1fee6d34f6268b141febc7cba3", eid:1, showMenu:"true", mrt:300,sis:0,asv:1,mv:0, payload:"{\"userID\":\"<?php echo $_SESSION['participantSession']; ?>\",\"Answer\":\"PossibleAnswer1_Question2\"}"};
-(function() {var pipe = document.createElement('script'); pipe.type = 'text/javascript'; pipe.async = true;pipe.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 's1.addpipe.com/1.3/pipe.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pipe, s);})();
-</script>
-<div id="hdfvr-content" ></div>
-<!-- end video recorder code -->
-
-</p>
-</div>
 
 
 <input type="hidden" name="projectid" id="projectid" value="<?php echo $rowproject['ProjectID']; ?>"/>
 <div class="popupoverlay-btn">
   <div class="cancel-delete">
-    <button class="slide_close cancel">Close</button>
+    <button class="slide-video-feedback_close cancel">Close</button>
 </div>
 
 <div class="popupoverlay-btn">
   <div class="close-delete">
-    <button class="slide_close cancel">Close</button>
+    <button class="slide-video-feedback_close cancel">Close</button>
 </div>
 </div>
 
 </div>
 </div>
 
-<!-- End Delete a Project -->
+<!-- End Possible Answer Question 1 -->
+
+
+
 
 
 
@@ -845,7 +817,7 @@ if($participant_home->is_logged_in())
 $(document).ready(function () {
 
 
-$(document).on("click", ".slide_open", function () {
+$(document).on("click", ".slide-video-feedback_open", function () {
 
   
 
@@ -854,16 +826,11 @@ $(document).on("click", ".slide_open", function () {
      //alert(selected);
      
      if(selected == 'PossibleAnswer1_Question1'){
-      alert(selected);
-     $("#PossibleAnswer1_Question2").hide();
-     $("#PossibleAnswer1_Question1").show();
-     return false;
+     $("#frame-video-feedback").attr("src", "../video1.php");
      }
 
      if(selected == 'PossibleAnswer1_Question2'){
-     $("#PossibleAnswer1_Question2").show();
-     $("#PossibleAnswer1_Question1").hide();
-     return false;
+    $("#frame-video-feedback").attr("src", "../video2.php");
      }
      // As pointed out in comments, 
      // it is superfluous to have to manually call the modal.
@@ -871,11 +838,13 @@ $(document).on("click", ".slide_open", function () {
 });
 
 
-    $('#slide').popup({
+    $('#slide-video-feedback').popup({
         focusdelay: 400,
         outline: true,
         vertical: 'top'
     });
+
+    
 
 });
 </script>
--- pos/wantrepreneur/problem/p/questions_answer1.php
+++ pos/wantrepreneur/problem/p/questions_answer1.php
@@ -11,28 +11,30 @@ if($_GET['projectid'] != ''){
 $sqlproject = mysqli_query($connecDB,"SELECT * FROM tbl_startup_project  WHERE ProjectID = '".$_GET['projectid']."' ");
 $rowproject = mysqli_fetch_array($sqlproject);	
 
-//echo "<h3>Thank you!</h3>";
+?>
 
-echo '<div class="space"></div>';
+<!--<h3>Thank you!</h3>-->
 
-echo "<h3>You answered: <strong>Yes, I have that problem</strong></h3>";
+<div class="space"></div>
 
-echo '<div class="space"></div>';
+<h3>You answered: <strong>Yes, I have that problem</strong></h3>
 
-echo '<div class="col-lg-12">';
+<div class="space"></div>
 
-echo '<div class="col-lg-12">';
-echo "<h4>Please provide your feedback for the following questions:</h4>";
-echo '</div>';
+<div class="col-lg-12">
 
-echo '<div class="space"></div>';
+<div class="col-lg-12">
+<h4>Please provide your feedback for the following questions:</h4>
+</div>
 
-echo '</div>';
+<div class="space"></div>
+
+</div>
 
 
-echo '<div class="col-lg-11">';
-echo '<div id="questions">';
-echo '<table class="table table-striped">
+<div class="col-lg-11">
+<div id="questions">
+<table class="table table-striped">
     <tbody>
       <tr>
         <td style="text-align:left;"><strong>Question 1</strong></td>
@@ -40,20 +42,23 @@ echo '<table class="table table-striped">
     </tr>
 
    <tr>
-        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10">'.$rowproject['PossibleAnswer1_Question1'].'</td>
+        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10"><?php echo $rowproject['PossibleAnswer1_Question1']; ?></td>
         <td style="text-align:left;" class="col-lg-2">
-<a data-toggle="modal" data-id="PossibleAnswer1_Question1" title="Add this item" class="slide_open btn-primary" href="#">Record Feedback</a>
+<a data-toggle="modal" data-id="PossibleAnswer1_Question1" title="Add this item" class="slide-video-feedback_open btn-primary" href="#">Record Feedback</a>
        </td>
     </tr>
-</table>';
-echo '</div>'; 
-echo '</div>';   
+</table>
+</div>
+</div>
 
 
 
-echo '<div class="col-lg-11">';
-echo '<div id="questions">';
-echo '<table class="table table-striped">
+
+    
+
+<div class="col-lg-11">
+<div id="questions">
+<table class="table table-striped">
     <tbody>
       <tr>
         <td style="text-align:left;"><strong>Question 2</strong></td>
@@ -61,23 +66,19 @@ echo '<table class="table table-striped">
     </tr>
 
    <tr>
-        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10">'.$rowproject['PossibleAnswer1_Question2'].'</td>
-       <td style="text-align:left;" class="col-lg-2">
-<a data-toggle="modal" data-id="PossibleAnswer1_Question2" title="Add this item" class="slide_open btn-primary" href="#">Record Feedback</a>
-       
-
-
+        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10"><?php echo $rowproject['PossibleAnswer1_Question2']; ?></td>
+        <td style="text-align:left;" class="col-lg-2">
+<a data-toggle="modal" data-id="PossibleAnswer1_Question2" title="Add this item" class="slide-video-feedback_open btn-primary" href="#">Record Feedback</a>
        </td>
     </tr>
-</table>';
-echo '</div>'; 
-echo '</div>';        
-    
+</table>
+</div>
+</div>
 
 
-echo '<div class="col-lg-11">';
-echo '<div id="questions">';
-echo '<table class="table table-striped">
+<div class="col-lg-11">
+<div id="questions">
+<table class="table table-striped">
     <tbody>
       <tr>
         <td style="text-align:left;"><strong>Question 3</strong></td>
@@ -85,18 +86,19 @@ echo '<table class="table table-striped">
     </tr>
 
    <tr>
-        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10">'.$rowproject['PossibleAnswer1_Question3'].'</td>
-        <td style="text-align:left;" class="col-lg-2">Feedback</td>
+        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10"><?php echo $rowproject['PossibleAnswer1_Question3']; ?></td>
+        <td style="text-align:left;" class="col-lg-2">
+<a data-toggle="modal" data-id="PossibleAnswer1_Question1" title="Add this item" class="slide1_open btn-primary" href="#">Record Feedback</a>
+       </td>
     </tr>
-</table>';
-echo '</div>'; 
-echo '</div>';   
+</table>
+</div>
+</div>
 
 
-
-echo '<div class="col-lg-11">';
-echo '<div id="questions">';
-echo '<table class="table table-striped">
+<div class="col-lg-11">
+<div id="questions">
+<table class="table table-striped">
     <tbody>
       <tr>
         <td style="text-align:left;"><strong>Question 4</strong></td>
@@ -104,14 +106,18 @@ echo '<table class="table table-striped">
     </tr>
 
    <tr>
-        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10">'.$rowproject['PossibleAnswer1_Question4'].'</td>
-        <td style="text-align:left;" class="col-lg-2">Feedback</td>
+        <td style="text-align:left; border-right:1px solid #eee"" class="col-lg-10"><?php echo $rowproject['PossibleAnswer1_Question1']; ?></td>
+        <td style="text-align:left;" class="col-lg-2">
+<a data-toggle="modal" data-id="PossibleAnswer1_Question1" title="Add this item" class="slide1_open btn-primary" href="#">Record Feedback</a>
+       </td>
     </tr>
-</table>';
-echo '</div>'; 
-echo '</div>';   
+</table>
+</div>
+</div>
 
 
+
+<?php 
 
 
 }
