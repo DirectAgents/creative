6a03f3c5d19f7b3f2495058b21ad4dd6acf74eee
Alper Dilmen <ald183s@gmail.com>
2018-04-04 16:13:32 -0400

    asdfsaf

--- pos/video/choose/welcome-email.php
+++ pos/video/choose/welcome-email.php
@@ -11,6 +11,7 @@ $subject = "Welcome";
 $to = new SendGrid\Email('', $_SESSION['email']);
 $content = new SendGrid\Content("text/html", '
          
+
 <body style="margin: 0 !important; padding: 0 !important;">
 
 
@@ -26,8 +27,8 @@ $content = new SendGrid\Content("text/html", '
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-top:50px; max-width: 600px;" class="wrapper">
                 <tr>
                     <td align="left" valign="top" style="padding:20px;" class="logo">
-                        <a href="http://litmus.com" target="_blank">
-                            <img alt="Logo" src="http://valifyit.com/images/email/email-logo-large.jpg" width="132" height="48" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
+                        <a href="https://misterpao.com/" target="_blank">
+                            <img alt="Logo" src="https://misterpao.com/images/email/email-logo-large.png" width="264" height="73" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
                         </a>
                     </td>
                 </tr>
@@ -70,49 +71,17 @@ $content = new SendGrid\Content("text/html", '
 
                                         <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
                                             <tr>
-                                                 <td align="center" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
-                                              
-                                              <a href="javascript:void(0)">
-                                    
-								'.$image.'
-
-</a>
-                                              
-                                              
+                                                 <td align="left" style="padding: 0 0 5px 25px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
+                                                You\'re on your way!
                                                 </td>
                                             </tr>
-
-                                             <tr>
-                                                 <td align="center" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
-                                                '.$row['Fullname'].'
-                                                </td>
-                                            </tr>
-
-                                               <tr>
-                                                 <td align="center" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
-                                                '.str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($row['City'])))).', '.$row['State'].'
-                                                </td>
-                                            </tr>
-
-
-                                            <tr>
-                                                 <td align="center" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
-                                               &nbsp;
-                                                </td>
-                                            </tr>
-
-
-                                              <tr>
-                                                 <td align="center" style="padding: 0 0 5px 25px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
-
-                                              <a href="'.BASE_PATH.'/connections/" style="text-decoration:none; color:#fff; background:#ff7676; padding:10px; border: 1px solid #ff7676; border-radius: 3px;">Connect +</a>
-
-                                                </td>
-                                            </tr>
-
-                                             
                                             
-                                             
+                                             <tr>
+                                                 <td align="left" style="padding: 0 0 5px 25px;font-size: 22px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #333333;" class="padding">
+                                                Let\'s confirm your email address.
+                                                
+                                                </td>
+                                            </tr>
                                         </table>
                                     </div>
                                    
@@ -122,6 +91,37 @@ $content = new SendGrid\Content("text/html", '
 
 
 
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600;">
+                            <tbody><tr>
+                                <td align="center" valign="top" style="font-size:0;">
+                                    <!--[if (gte mso 9)|(IE)]>
+                                    <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
+                                    <tr>
+                                    <td align="left" valign="top" width="115">
+                                    <![endif]-->
+                                    <div style="display:inline-block; margin: 0 -2px; max-width:600px; vertical-align:top; width:100%;">
+
+                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
+                                            <tbody>
+                                              <tr>
+                                                <td valign="top" align="center" style="padding: 40px 0 0 0; text-decoration:none" class="mobile-hide">
+                                                
+                                                 <a href="'.BASE_PATH.'/account/verify.php?id='.$id.'&code='.$code.'">
+                                                <div style="padding: 20px; max-width:240px; text-decoration:none !important; text-decoration:none; font-size: 16px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; background:#348eda; color: #ffffff; text-decoration: none !important;" class="padding">
+                                                <img alt="Logo" src="https://misterpao.com/images/email/confirm-email-address.png" width="219" height="15" style="display: block; border="0">
+                                                </div>
+                                                </a>
+                                                
+                                                </td>
+                                              </tr>
+                                          </tbody>
+                                        </table>
+                                    </div>
+                                
+                                </td>
+                            </tr>
+                        </tbody></table>
+                        
                         
                         
                         
@@ -170,7 +170,7 @@ $content = new SendGrid\Content("text/html", '
                <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="max-width: 600px;" class="responsive-table">
                 <tr>
                     <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
-                        <img alt="Logo" src="http://valifyit.com/images/email/email-logo-small.jpg" width="110" height="34" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
+                        <img alt="Logo" src="https://misterpao.com/images/email/email-logo-small.jpg" width="150" height="41" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
                            </td>
                      </tr>
 
@@ -180,15 +180,24 @@ $content = new SendGrid\Content("text/html", '
 
 
             <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="max-width: 600px;" class="responsive-table">
+
+             <tr>
+                    <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
+                       &nbsp;
+                           </td>
+                     </tr>
+
+                <tr>
+                
                 <tr>
                     <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
-                        245 5th Ave Suite 201, New York, NY 10001
+                       245 5th Ave Suite 201, New York, NY 10001
                            </td>
                      </tr>
 
                       <tr>
                       <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">   
-                        <a href="http://valifyit.com/terms/" target="_blank" style="color: #666666; text-decoration: none;">Terms of Service</a> | <a href="http://valifyit.com/privacy/" target="_blank" style="color: #666666; text-decoration: none;">Privacy</a>  | <a href="http://valifyit.com/faq/" target="_blank" style="color: #666666; text-decoration: none;">FAQ</a></td>
+                        <a href="https://misterpao.com/terms/" target="_blank" style="color: #666666; text-decoration: none;">Terms of Service</a> | <a href="https://misterpao.com/privacy/" target="_blank" style="color: #666666; text-decoration: none;">Privacy</a>  | <a href="https://misterpao.com/faq/" target="_blank" style="color: #666666; text-decoration: none;">FAQ</a></td>
                        
                         
  
@@ -206,7 +215,6 @@ $content = new SendGrid\Content("text/html", '
         </td>
     </tr>
 </table>
-
 
 </body>
 </html>
